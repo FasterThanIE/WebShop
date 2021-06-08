0a41e2a76e498d570092d8e2fb79be34e6f0be62
Dusan Simic <dusansimic202gmail.com>
2021-06-08 20:40:23 +0200

    small changes

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -89,7 +89,7 @@
                                         {{ __('Logout') }}
                                     </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="get" class="d-none">
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
                                 </div>
